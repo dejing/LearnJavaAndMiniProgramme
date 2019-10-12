1bb3ab11a525e641263e2b51a199064f11e09b19
dejing <66173469@qq.com>
2019-10-12 09:02:59 +0800

    MyBatis+MySQL读写分离

---
+++ src/main/java/com/dejing/miniprogram/mapper/article/ArticleDao.java
@@ -0,0 +1,30 @@
+package com.dejing.miniprogram.mapper.article;
+
+import com.dejing.miniprogram.mapper.article.Article;
+import com.dejing.miniprogram.mapper.article.ArticleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ArticleDao {
+    long countByExample(ArticleExample example);
+
+    int deleteByExample(ArticleExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Article record);
+
+    int insertSelective(Article record);
+
+    List<Article> selectByExample(ArticleExample example);
+
+    Article selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByExample(@Param("record") Article record, @Param("example") ArticleExample example);
+
+    int updateByPrimaryKeySelective(Article record);
+
+    int updateByPrimaryKey(Article record);
+}
